5d31ad881a585b9880072307c9c9c6148bdc75bd
Arlindo Vinicius Amado Nascimento Silva <viniciusamado7@gmail.com>
2022-11-12 14:04:07 -0300

    Deletando arquivos que não estão sendo utilizados

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,12 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Router from "./routes/Router";
 import { ThemeProvider } from "@mui/material";
 import theme from "./constants/theme";
 
 const App = () => {
-  useEffect(() => {}, []);
-
   return (
     <ThemeProvider theme={theme}>
       <Router />
